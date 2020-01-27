a3b41b9205347d370da88dc2ef3670b2af1178e1
FabianSchuetze <fabian.schutze@eui.eu>
2020-01-27 17:19:53 +0100

    first run of functions

--- functions.cu
+++ functions.cu
@@ -1,5 +1,15 @@
 #include <stdbool.h>
-void merge_sequential(int* A, int m, int* B, int n, int* C) {
+#include <stdio.h>
+#define CHECK(call)                                                \
+    {                                                              \
+        const cudaError_t error = call;                            \
+        if (error != cudaSuccess) {                                \
+            fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__); \
+            fprintf(stderr, "code: %d, reason: %s\n", error,       \
+                    cudaGetErrorString(error));                    \
+        }                                                          \
+    }
+__device__ void merge_sequential(int* A, int m, int* B, int n, int* C) {
     int i = 0; //index A
     int j = 0; // index B
     int k = 0; // index C
@@ -24,7 +34,7 @@ void merge_sequential(int* A, int m, int* B, int n, int* C) {
     }
 }
 
-int co_rank(int k, int*A, int m, int* B,int n) {
+__device__ void co_rank(int k, int*A, int m, int* B,int n, int* out) {
     int i = k < m ? k : m;
     int j = k - i;
     int i_low = 0 > (k-n) ? 0 : k-n;
@@ -32,7 +42,7 @@ int co_rank(int k, int*A, int m, int* B,int n) {
     int delta;
     bool active = true;
     while (active) {
-        if (i > 0 && j < n && A[i-1] > B[i]) {
+        if (i > 0 && j < n && A[i-1] > B[j]) {
             delta = ((i-i_low + 1) >> 1);
             j_low = j;
             j = j + delta;
@@ -46,18 +56,58 @@ int co_rank(int k, int*A, int m, int* B,int n) {
             active = false;
         }
     }
-    return i;
+    out[0] = i;
 }
-
 
 __global__ void merge_basic_kernel(int* A, int m, int* B, int n, int* C) {
     int tid = blockIdx.x*blockDim.x + threadIdx.x;
-    int k_curr = tid*ceil((m+n) / (blockDim.x*gridDim.x));
-    int k_next = min((tid+1)*ceil((m+n) / (blockDim.x * gridDim.x)), m+n);
-    int i_curr = co_rank(k_curr, A, m, B,n);
-    int i_next = co_rank(k_next, A, m, B, n);
+    //printf("the tid is: %i\n", tid);
+    float sum = m + n;
+    int k_curr = tid*ceilf((sum) / (blockDim.x*gridDim.x));
+    int k_next = min((tid+1)*ceilf((m+n) / (blockDim.x * gridDim.x)), sum);
+    //printf("k_curr %i, k_next %i and tid: %i\n", k_curr, k_next, tid);
+    int i_curr;
+    int i_next;
+    co_rank(k_curr, A, m, B,n, &i_curr);
+    co_rank(k_next, A, m, B, n, &i_next);
     int j_curr = k_curr - i_curr;
     int j_next = k_next - i_next;
     merge_sequential(&A[i_curr], i_next - i_curr, &B[j_curr], 
                      j_next-j_curr, &C[k_curr]);
+}
+
+int main() {
+    int* A;
+    int* B;
+    int* C;
+    A = new int[5];
+    B = new int[4];
+    C = new int[9];
+    A[0] = 1;
+    A[2] = 7;
+    A[3] = 8;
+    A[4] = 9;
+    A[5] = 10;
+    B[0] = 7;
+    B[1] = 10;
+    B[2] = 10;
+    B[3] = 12;
+    int * d_A;
+    int * d_B;
+    int * d_C;
+    CHECK(cudaMalloc((void**)&d_A, 5 * sizeof(int)));
+    CHECK(cudaMalloc((void**)&d_B, 4 * sizeof(int)));
+    CHECK(cudaMalloc((void**)&d_C, 9 * sizeof(int)));
+    CHECK(cudaMemcpy(d_A, A, 5 * sizeof(int), cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_B, B, 4 * sizeof(int), cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_C, C, 9 * sizeof(int), cudaMemcpyHostToDevice));
+    dim3 blockDim(1);
+    dim3 gridDim(3);
+    merge_basic_kernel<<<blockDim, gridDim>>>(d_A, 5, d_B, 4, d_C);
+    CHECK(cudaDeviceSynchronize());
+    printf("Completed\n");
+    CHECK(cudaMemcpy(C, d_C, 9 * sizeof(int), cudaMemcpyDeviceToHost));
+    for (int i = 0; i < 9; ++i) {
+        printf("at %i we have %i\n",i, C[i]);
+    }
 }
