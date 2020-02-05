4c0b70220ca2423cb45a84f5f9c30b31ff790199
FabianSchuetze <fabian.schutze@eui.eu>
2020-02-05 19:48:48 +0100

    seems to be relatively robust

--- src/copy.cu
+++ src/copy.cu
@@ -6,8 +6,8 @@ __device__ void copy_shared(const int* d_A, int sz_a, int* shared,
     int start = threadIdx.x;
     while (start < shared_size &&
            block_pos + start < n_block_ele * (blockIdx.x + 1)) {
-        shared[block_pos + start] = d_A[block_pos + start];
-        d_C[block_pos + start] = shared[block_pos + start];
+        shared[start] = d_A[block_pos + start];
+        d_C[block_pos + start] = shared[start];
         start += blockDim.x;
     }
 }
@@ -22,13 +22,15 @@ __global__ void copy(const int* d_A, int sz_a, int* d_C, const int shared_sz) {
 void copy_cuda(const int* d_A, int sz_a, int* d_C) {
     cudaDeviceProp dev_prop;
     cudaGetDeviceProperties(&dev_prop, 0);
-    int size_shared = dev_prop.sharedMemPerBlock;
-    printf("The size of shared vs a + b is: %i  vs %i\n", size_shared,
-           sz_a * sizeof(int));
-    dim3 blockDim(2);
-    dim3 gridDim(3);  // ten threads, likely bug is too many selected
+    //int size_shared = dev_prop.sharedMemPerBlock;
+    //printf("The size of shared vs a + b is: %i  vs %i\n", size_shared,
+           //sz_a * sizeof(int));
+    dim3 blockDim(10);
+    dim3 gridDim(100);  // ten threads, likely bug is too many selected
+    int size_shared = 10000;
     /*double beg = cpuSecond();*/
-    copy<<<blockDim, gridDim, 10 * sizeof(int)>>>(d_A, sz_a, d_C, 10);
+    copy<<<gridDim, blockDim,  size_shared* sizeof(int)>>>(d_A, sz_a, d_C, 
+                                                           size_shared);
     // double end = cpuSecond() - beg;
     MY_CHECK(cudaDeviceSynchronize());
     MY_CHECK(cudaPeekAtLastError());
