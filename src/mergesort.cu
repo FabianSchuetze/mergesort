58ca8af4fb2314b9a21af9e94d6510d83eb848b7
FabianSchuetze <fabian.schutze@eui.eu>
2020-02-06 18:57:27 +0100

    Has function for coarse range

--- src/mergesort.cu
+++ src/mergesort.cu
@@ -1,13 +1,10 @@
-#include "../include/merge.h"
-#include "../include/common.h"
-#include <sys/time.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include "../include/common.h"
+#include "../include/merge.h"
 
-double cpuSecond() {
-    struct timeval tp;
-    gettimeofday(&tp, NULL);
-    return ((double)tp.tv_sec + (double)tp.tv_usec * 1e-6);
-}
+#include "../include/merge.h"
+
 __device__ int mergepath(const int* a, int size_a, const int* b, int size_b,
                          int diag) {
     if (diag == 0) return 0;
@@ -44,53 +41,60 @@ __device__ void merge(const int* a, int start_a, int sz_a, const int* b,
     }
 }
 
-//__device__ void allocate_shared_memory(const int* a, int sz_a, const int* b,
-// int* shared, int length) {
-// int process = blockIdx.x * blockDim.x + threadIdx.x;
-// int diag = process * length;
-// for (int i = 0; i < length; ++i) {
-// shared[i + diag] = a[i + diag];
-// shared[sz_a + i + diag] = b[i + diag];
-//}
-////__syncthreads();
-//}
-
 __global__ void paralleMerge(const int* a, int sz_a, const int* b, int sz_b,
                              int* c, int length) {
+    /*int process = */
     int process = blockIdx.x * blockDim.x + threadIdx.x;
     int diag = process * length;
-    extern __shared__ int shared[];
-    for (int i = 0; i < length; ++i) {
-        int pos = (i + diag) / 2;
-        if (blockIdx.x == 5) {
-            printf("pos %i\n", pos);
-        }
-        shared[pos] = a[pos];
-        shared[sz_a + pos] = b[pos];
-    }
-    __syncthreads();
-    // void allocate_shared_memory(a, sz_a, b, shared, length);
-    int a_start = mergepath(shared, sz_a, &shared[sz_a], sz_b, diag);
+    int a_start = mergepath(a, sz_a, b, sz_b, diag);
     int b_start = diag - a_start;
-    merge(shared, a_start, sz_a, &shared[sz_a], b_start, sz_b, c, diag, length);
+    merge(a, a_start, sz_a, b, b_start, sz_b, c, diag, length);
 }
 
+__global__ void cuda_determine_range(const int* a, int sz_a, const int* b,
+                                     int sz_b, int shared_size,
+                                     int* block_boundaries) {
+    int diag = shared_size * threadIdx.x;
+    printf("the diag is %i, at thread %i with blockDim %i\n", diag, threadIdx.x,
+           blockDim.x);
+    if (diag < sz_a + sz_b) {
+        int a_start = mergepath(a, sz_a, b, sz_b, diag);
+        block_boundaries[threadIdx.x] = a_start;
+        block_boundaries[threadIdx.x + blockDim.x + 1] = diag - a_start;
+        printf("The range for thread %i at blockDim %i is %i\n", threadIdx.x,
+               blockDim.x, a_start);
+    }
+}
+void determine_range(const int* d_A, int sz_a, const int* d_B, int sz_b,
+                     int size_shared, const int* boundaries,
+                     int* d_boundaries) {
+    int n_blocks = ceilf((sz_a + sz_b) / size_shared);
+    cudaMalloc((void**)&d_boundaries, 2 * n_blocks * sizeof(int));
+    cudaMemcpy(d_boundaries, boundaries, 2 * n_blocks * sizeof(int),
+               cudaMemcpyHostToDevice);
+    cuda_determine_range<<<1, gridDim>>>(d_A, sz_a, d_B, sz_b, size_shared,
+                                         d_boundaries);
+}
 void cuda_merge(const int* d_A, int sz_a, const int* d_B, int sz_b, int* d_C,
-                int processes) {
-    cudaDeviceProp dev_prop;
-    cudaGetDeviceProperties(&dev_prop, 0);
-    int size_shared =  dev_prop.sharedMemPerBlock;
-    int number_blocks = (sz_a + sz_b) * sizeof(int) / size_shared + 10;
-    //int size_shared = (sz_a + sz_b) * 2 * sizeof(int);
-    int length = (sz_a + sz_b) / (processes * number_blocks);
-    printf("the length is %i\n", length);
-    dim3 blockDim(number_blocks);
-    dim3 gridDim(processes);  // ten threads, likely bug is too many selected
-    double beg = cpuSecond();
-    paralleMerge<<<blockDim, gridDim, size_shared>>>(d_A, sz_a, d_B, sz_b, d_C,
-                                                     length);
-    double end = cpuSecond() - beg;
-    MY_CHECK(cudaDeviceSynchronize());
-    MY_CHECK(cudaPeekAtLastError());
-    printf("The operation took %.5f\n", end);
+                int length) {
+    dim3 blockDim(10);
+    int size_shared = 10;
+    int n_blocks = ceilf((sz_a + sz_b) / size_shared);
+    dim3 gridDim(n_blocks);  // ten threads, likely bug is too many selected
+    int boundaries[2 * n_blocks + 2];
+    int* d_boundaries;
+    determine_range(d_A, sz_a, d_B, sz_b, size_shared, boundaries,
+                    d_boundaries);
+    // boundaries[n_blocks] = sz_a;
+    // boundaries[2 * n_blocks] = sz_b;
+    // cudaMemcpy(boundaries, d_boundaries, 2 * n_blocks * sizeof(int),
+    // cudaMemcpyDeviceToHost);
+    // MY_CHECK(cudaDeviceSynchronize());
+    for (int i = 0; i < n_blocks; ++i) {
+        printf("At block %i the range of a is (%i, %i) and of b is (%i, %i)\n",
+               i, boundaries[i], boundaries[i + 1], boundaries[i + n_blocks],
+               boundaries[i + 1 + n_blocks]);
+    }
+    paralleMerge<<<gridDim, blockDim>>>(d_A, sz_a, d_B, sz_b, d_C, length,
+                                        d_boundaries);
 }
