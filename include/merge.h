211999eec651bc0a071f8fba8a7225f43cafed40
FabianSchuetze <fabian.schutze@eui.eu>
2020-02-15 17:16:24 +0100

    End of day

--- include/merge.h
+++ include/merge.h
@@ -1,7 +1,7 @@
 #ifndef merge_h
 #define merge_h
 double cuda_merge_global(int* A, int m, int* B, int n, int* C, int);
-double cuda_merge(int* A, int m, int* B, int n, int* C, int);
+double cuda_merge(int* A, int m, int* B, int n, int* C);
 void copy_cuda(const int* A, int m, int* C);
 void merge(const int* A, int m, int n, int* C);
 #endif
--- src/device_prop.cu
+++ src/device_prop.cu
@@ -3,7 +3,12 @@
 int main() {
     cudaDeviceProp dev_prop;
     cudaGetDeviceProperties(&dev_prop, 0);
-    printf("The maximum amount of threads is %i\n", dev_prop.maxThreadsPerBlock);
+    printf("Comput cappbility %i and %i\n", dev_prop.major, dev_prop.minor);
+    printf("SP count %i\n", dev_prop.multiProcessorCount);
+    printf("The maximum amount of threads per Block %i\n",
+            dev_prop.maxThreadsPerBlock);
+    printf("The maximum amount of threads per SM %i\n",
+            dev_prop.maxThreadsPerMultiProcessor);
     printf("The maximum global memory is %zu\n", dev_prop.totalGlobalMem);
     printf("The total number of SM is %i\n", dev_prop.multiProcessorCount);
     printf("The number of register per block is %i\n", dev_prop.regsPerBlock);
@@ -11,5 +16,7 @@ int main() {
             dev_prop.sharedMemPerBlock);
     printf("The amount of shared memory per SM %zu\n",
             dev_prop.sharedMemPerMultiprocessor);
+    printf("  Peak Memory Bandwidth (GB/s): %f\n\n",
+           dev_prop.memoryClockRate*(dev_prop.memoryBusWidth/8)/1.0e6);
 
 }
